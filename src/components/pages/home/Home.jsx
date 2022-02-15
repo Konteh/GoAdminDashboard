787a4dec7d22dd25790d2208d1be72e5158eac2c
Konteh <kontehebrima993@gmail.com>
2022-02-15 03:51:52 -0800

    add user Analytics chart

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,10 +1,13 @@
-import FeaturedInfo from '../../featuredInfo/FeaturedInfo'
-import './home.css'
 
+import Chart from '../../chart/Chart';
+import FeaturedInfo from '../../featuredInfo/FeaturedInfo';
+import './home.css';
+import {userData} from "../../.././dummyData"
 export default function Home() {
     return (
         <div className="home">
             <FeaturedInfo></FeaturedInfo>
+            <Chart data={userData} title="User Analytics" grid dataKey="ActiveUser"/>
         </div>
     )
 }
---
+++ src/dummyData.js
@@ -0,0 +1,66 @@
+export const userData = [
+    {
+      name: 'Jan',
+      "ActiveUser": 7000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: 'Feb',
+      "ActiveUser": 7000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: 'Mar',
+      "ActiveUser": 7000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: 'Apr',
+      "ActiveUser": 4000,
+      
+    },
+    {
+      name: 'May',
+      "ActiveUser": 5000,
+      
+    },
+    {
+      name: 'Jun',
+      "ActiveUser": 3000,
+      
+    },
+    {
+      name: 'Jul',
+      "ActiveUser": 5000,
+     
+    },
+    {
+      name: 'Aus',
+      "ActiveUser": 2000,
+     
+    },
+    {
+      name: 'Sep',
+      "ActiveUser": 7000,
+   
+    },
+    {
+      name: 'Oct',
+      "ActiveUser": 5000,
+     
+    },
+    {
+      name: 'Nov',
+      "ActiveUser": 4000,
+      
+    },
+    {
+      name: 'Dec',
+      "ActiveUser": 6000,
+      
+    },
+    
+  ];
