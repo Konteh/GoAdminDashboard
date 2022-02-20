6957965d5137e2f16319266962b917b55477af14
Konteh <kontehebrima993@gmail.com>
2022-02-20 14:42:08 -0800

    create user table and enable user delete user funtionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import UserList from "./components/pages/userList/UserList";
 import "./app.css"
 import Home from "./components/pages/home/Home";
 const { default: Sidebar } = require("./components/topbar/sidebar/sidebar");
@@ -6,14 +12,19 @@ const { default: Topbar } = require("./components/topbar/Topbar");
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
     <Topbar/>
     <div className="container">
       <Sidebar/>
-      <Home></Home>
+      <Routes>
+      <Route path="/" element={<Home />} />
+        <Route path="users/" element={<UserList />} />
+        {/* <Route path='/' element={<Home/>} /> */}
+        
+      </Routes>
       
     </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
--- src/dummyData.js
+++ src/dummyData.js
@@ -64,3 +64,57 @@ export const userData = [
     },
     
   ];
+
+  export const userRows = [
+    { id: 1, username:"conteh", avatar: "https://media-exp1.licdn.com/dms/image/C4D03AQGnvL_GtxI7SQ/profile-displayphoto-shrink_200_200/0/1639439250502?e=1649894400&v=beta&t=yz9XnyeErXZnbmZ2GvLgFpGqajr52YtWT7ayQAxxlyA",
+    email:"mathema@gmail.com",
+    status:"active" ,
+    transaction: "D299.00"    
+},
+    { id: 2, username:"conteh", avatar: "https://media-exp1.licdn.com/dms/image/C4D03AQGnvL_GtxI7SQ/profile-displayphoto-shrink_200_200/0/1639439250502?e=1649894400&v=beta&t=yz9XnyeErXZnbmZ2GvLgFpGqajr52YtWT7ayQAxxlyA",
+    email:"mathema@gmail.com",
+    status:"active" ,
+    transaction: "D299.00"    
+},
+    { id: 3, username:"conteh", avatar: "https://media-exp1.licdn.com/dms/image/C4D03AQGnvL_GtxI7SQ/profile-displayphoto-shrink_200_200/0/1639439250502?e=1649894400&v=beta&t=yz9XnyeErXZnbmZ2GvLgFpGqajr52YtWT7ayQAxxlyA",
+    email:"mathema@gmail.com",
+    status:"active" ,
+    transaction: "D299.00"    
+},
+    { id: 4, username:"conteh", avatar: "https://media-exp1.licdn.com/dms/image/C4D03AQGnvL_GtxI7SQ/profile-displayphoto-shrink_200_200/0/1639439250502?e=1649894400&v=beta&t=yz9XnyeErXZnbmZ2GvLgFpGqajr52YtWT7ayQAxxlyA",
+    email:"mathema@gmail.com",
+    status:"active" ,
+    transaction: "D299.00"    
+},
+    { id: 5, username:"conteh", avatar: "https://media-exp1.licdn.com/dms/image/C4D03AQGnvL_GtxI7SQ/profile-displayphoto-shrink_200_200/0/1639439250502?e=1649894400&v=beta&t=yz9XnyeErXZnbmZ2GvLgFpGqajr52YtWT7ayQAxxlyA",
+    email:"mathema@gmail.com",
+    status:"active" ,
+    transaction: "D299.00"    
+},
+    { id: 6, username:"conteh", avatar: "https://media-exp1.licdn.com/dms/image/C4D03AQGnvL_GtxI7SQ/profile-displayphoto-shrink_200_200/0/1639439250502?e=1649894400&v=beta&t=yz9XnyeErXZnbmZ2GvLgFpGqajr52YtWT7ayQAxxlyA",
+    email:"mathema@gmail.com",
+    status:"active" ,
+    transaction: "D299.00"    
+},
+    { id: 7, username:"conteh", avatar: "https://media-exp1.licdn.com/dms/image/C4D03AQGnvL_GtxI7SQ/profile-displayphoto-shrink_200_200/0/1639439250502?e=1649894400&v=beta&t=yz9XnyeErXZnbmZ2GvLgFpGqajr52YtWT7ayQAxxlyA",
+    email:"mathema@gmail.com",
+    status:"active" ,
+    transaction: "D299.00"    
+},
+    { id: 8, username:"conteh", avatar: "https://media-exp1.licdn.com/dms/image/C4D03AQGnvL_GtxI7SQ/profile-displayphoto-shrink_200_200/0/1639439250502?e=1649894400&v=beta&t=yz9XnyeErXZnbmZ2GvLgFpGqajr52YtWT7ayQAxxlyA",
+    email:"mathema@gmail.com",
+    status:"active" ,
+    transaction: "D299.00"    
+},
+    { id: 9, username:"conteh", avatar: "https://media-exp1.licdn.com/dms/image/C4D03AQGnvL_GtxI7SQ/profile-displayphoto-shrink_200_200/0/1639439250502?e=1649894400&v=beta&t=yz9XnyeErXZnbmZ2GvLgFpGqajr52YtWT7ayQAxxlyA",
+    email:"mathema@gmail.com",
+    status:"active" ,
+    transaction: "D299.00"    
+},
+    { id: 10, username:"conteh", avatar: "https://media-exp1.licdn.com/dms/image/C4D03AQGnvL_GtxI7SQ/profile-displayphoto-shrink_200_200/0/1639439250502?e=1649894400&v=beta&t=yz9XnyeErXZnbmZ2GvLgFpGqajr52YtWT7ayQAxxlyA",
+    email:"mathema@gmail.com",
+    status:"active" ,
+    transaction: "D299.00"    
+},
+   
+  ];
