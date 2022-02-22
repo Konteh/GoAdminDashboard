f1fa7c0cc27a45ce616c73a54aa6a3ae04f29aca
Konteh <kontehebrima993@gmail.com>
2022-02-21 17:14:45 -0800

    create data grid product list

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import "./app.css"
 import Home from "./components/pages/home/Home";
 import User from "./components/pages/user/User";
 import NewUser from "./components/pages/newUser/NewUser";
+import ProductList from "./components/pages/productList/ProductList";
 const { default: Sidebar } = require("./components/topbar/sidebar/sidebar");
 const { default: Topbar } = require("./components/topbar/Topbar");
 
@@ -25,6 +26,11 @@ function App() {
 
         <Route path="user/:userId" element={<User/>} />
         <Route path="newUser" element={<NewUser></NewUser>} />
+
+        <Route path="products/" element={<ProductList />} />
+
+        <Route path="product/:productsId" element={<User/>} />
+        <Route path="newproduct" element={<NewUser></NewUser>} />
         {/* <Route path='/' element={<Home/>} /> */}
         
       </Routes>
